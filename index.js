55fc779ddac560ebca2f658408c257908763bbc9
Keraunyx <58750868+Keraunyx@users.noreply.github.com>
2021-02-27 15:46:01 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,39 @@
+const fs = require('fs');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const { prefix, token } = require('./config.json');
+client.commands = new Discord.Collection();
+
+//looking at files that end with .js for commands
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+//looping through commands
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.name, command);
+}
+
+console.log(client.commands);
 
 client.once('ready', () => {
 	console.log('Ready!');
 });
 
+client.on('messaage', async message => {
+	if(!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).trim().split(/ +/);
+	const command = args.shift().toLowerCase();
+
+	if(!client.commands.has(command)) return;
+
+	try{
+		client.commands.get(command).execute(message, args);
+	} catch(error){
+		console.error(error);
+		message.reply('There was an error trying to execute that command!');
+	}
+});
+
 client.login(token);
+
