e0eccbf7e9def4c098b734ad71cc4f788163f998
theproz99 <bmitsevich@gmail.com>
2021-02-27 15:57:23 -0600

    help command update

--- commands/help.js
+++ commands/help.js
@@ -2,7 +2,6 @@ module.exports = {
     name: 'help',
     description: "helps people figure out what this bot can do.",
     execute(message, args) {
-        message.channel.send('List of commands: /n !help - displays list of commands. /n !disAssig [time] - dislpays upcoming assignments and due dates');
-        message.channel.send("/n !grades - DM's current user their grades ");
+        message.channel.send('List of commands: \n\t!help - displays list of commands. \n\t!disAssig [time] - dislpays upcoming assignments and due dates \n\t!grades - DM\'s current user their grades');
     }
 }
--- index.js
+++ index.js
@@ -19,7 +19,7 @@ client.once('ready', () => {
 	console.log('Ready!');
 });
 
-client.on('messaage', async message => {
+client.on('message', async message => {
 	if(!message.content.startsWith(prefix) || message.author.bot) return;
 
 	const args = message.content.slice(prefix.length).trim().split(/ +/);
