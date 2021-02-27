102ae20064483d3871e3b21fb321d4f6e4722cc5
theproz99 <bmitsevich@gmail.com>
2021-02-27 14:31:02 -0600

    Update index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.login('ODE1MzE1NzI4ODkzNzM5MDEw.YDqn8Q.cyTuGP3z86SVIPCUvv4s00vZjBk');
