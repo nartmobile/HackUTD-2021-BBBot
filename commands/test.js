02392e0250b7d4a0bf6230ef5b6ca693ea18b311
Andrew Tran <andtran0703@gmail.com>
2021-02-28 00:19:31 -0500

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -2,19 +2,15 @@ const mongoose = require('mongoose');
 const Servers = mongoose.model('Servers');
 
 module.exports = {
-    name: 'test',
-    description: "asdf.",
+    name: 'init',
+    description: "Initialize new collection for server.",
     execute: async (message, args) => {
 		message.channel.send(message.guild.id);
 		try {
 			await new Servers({
 				server_id: message.guild.id
 			}).save();
-	
-			message.channel.send("pp poopoo");
 		} catch {
-		} finally {
-			message.channel.send("pp poopoo1293");
 		}
     }
 }
